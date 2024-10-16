d5d58944ad83746208a856257c145d1578810f16
slimanfl <slimanite1996@gmail.com>
2024-10-16 05:42:54 -0700

    test2

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,16 @@ import { UserModule } from './user/user.module';
 @Module({
   imports: [
     UserModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'mysql.railway.internal',
-      port: 3306,
-      username: 'root',
-      password: 'QGCFXYjoarztnNgttCpcjMVCQxjkyFvL',
-      database: 'railway',
-      entities: ['dist/**/*.entity.{ts,js}'],
-      synchronize: true,
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'mysql.railway.internal',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'QGCFXYjoarztnNgttCpcjMVCQxjkyFvL',
+    //   database: 'railway',
+    //   entities: ['dist/**/*.entity.{ts,js}'],
+    //   synchronize: true,
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
